27bdd43055c04bf4ac65913d309b182e667d3200
CarlosDaniel661 <timonrock2015@gmail.com>
2025-03-03 17:51:07 -0300

    Switch to Chrome for CI tests

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 def get_driver():
-
     options = Options()
-    options.headless = True
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(executable_path="/usr/local/bin/geckodriver")
+    
+    service = Service(executable_path="/usr/local/bin/chromedriver")
 
-    driver = webdriver.Firefox(service=service, options=options)
+
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     return driver
