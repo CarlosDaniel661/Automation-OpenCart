69099e24933ade276b8a18f4054e11c67ac881f2
CarlosDaniel661 <timonrock2015@gmail.com>
2025-03-03 17:33:27 -0300

    Fix Geckodriver path for GitHub Actions

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-import time
 
 def get_driver():
     options = Options()
-    options.headless = False  
-    
-    service = Service(r"C:\dselenium\geckodriver.exe")
+    options.headless = True
+    #Geckodriver para entorno de linux
+    service = Service(executable_path="/usr/local/bin/geckodriver")
+
     driver = webdriver.Firefox(service=service, options=options)
-    
-    driver.implicitly_wait(5)  
-    
+    driver.maximize_window()
     return driver
