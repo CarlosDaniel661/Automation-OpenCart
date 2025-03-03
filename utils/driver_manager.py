53047b90f1866836be69140640eab9be681cb04f
CarlosDaniel661 <timonrock2015@gmail.com>
2025-03-03 17:43:29 -0300

    Fix Firefox and Geckodriver configuration

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -3,9 +3,10 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
 def get_driver():
+
     options = Options()
     options.headless = True
-    #Geckodriver para entorno de linux
+
     service = Service(executable_path="/usr/local/bin/geckodriver")
 
     driver = webdriver.Firefox(service=service, options=options)
